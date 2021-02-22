c872f4cc263fe88507f5694578542457e8126fba
王福朋 <wangfupeng1988@163.com>
2021-02-22 14:45:45 +0800

    chore: 环境

--- packages/app/lib/app.js
+++ packages/app/lib/app.js
@@ -1,13 +1,17 @@
 'use strict';
 
+const _ = require('lodash')
 const plugin1 = require('@wangfupeng1988/lerna-demo-plugin1')
 const plugin2 = require('@wangfupeng1988/lerna-demo-plugin2')
+
 
 console.log('app start...')
 
 function app() {
     plugin1()
     plugin2()
+
+    console.log('_.concat', _.concat([1, 2], 3))
 }
 
 app()
